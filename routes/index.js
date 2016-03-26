f224b54d12c468b2c2fff458566eecaafe3083cd
peterthesmsyt <th3smsyt@uw.edu>
2016-03-26 00:21:06 -0700

    Update with website template

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var express = require('express'),
 	path = require('path'),
 	__parentdirname = path.dirname(module.parent.filename);
 
+var fs = require('fs');
+
+var totalcount = 0;
 //Initial route to our views
 router.get('/', function(req, res) {
 	res.sendFile(__parentdirname + '/public/views/index.html');
@@ -12,8 +15,19 @@ router.get('/Azia', function(req, res) {
 	res.sendFile(__parentdirname + '/public/views/azia.html');
 });
 
-router.get('/PhoTai', function(req, res) {
-	res.send('Hello, This is a Pho Tai website');
-});
+router.get('/count/:ip', function(req, res) {
+	var userip = req.params.ip;
+	//totalcount = totalcount +ip 1;
+	totalcount++;
 
+	var holder =String(totalcount)+' '+String(userip)+'\n';
+	console.log('totalcount, userIP: '+holder);
+	fs.appendFile("count.txt", holder, function(err) {
+		if(err) {
+			return console.log(err);
+		}
+
+		console.log("The file was saved!");
+	});
+});
 module.exports = router;
